317f3cfc4051b3a48e87de4e65a678719c95f077
Peter Cai <peter@typeblog.net>
2020-12-25 19:00:13 +0800

    Disable Euicc when GMS and GSF are not installed
    * This avoids EuiccGoogle / GoogleCarrierServices crashing, taking the
      whole telephony stack with them when no GMS or GSF is installed.
    * Compared to change 274251, this approach is no-op when GAPPS add-on is
      flashed before the first boot, and thus should not cause issues with
      eSIM when GAPPS is flashed.
    * With microG, this also works after installing the microG GMS & GSF
      packages and rebooting, although manual activation of the eSIM is
      needed via Settings - Network.
    * The only downside is that users will still see one or two crashes
      during the first boot because our application only starts after the
      device is booted.
    
    Change-Id: I4a11c93e855978abbf8d84fdbe4ffc8b2aedc27c

--- device-lineage.mk
+++ device-lineage.mk
@@ -19,6 +19,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# Parts
+PRODUCT_PACKAGES += \
+    GoogleParts
+
 # Radio
 DEVICE_MANIFEST_FILE += \
     device/google/bluejay/manifest_radio.xml
