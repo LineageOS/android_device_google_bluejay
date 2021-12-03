47a2ff02d676767a3c2da9e4c33b6aa1c54f8726
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    bluejay: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -27,3 +27,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="bluejay-user 14 UP1A.231005.007 10754064 release-keys"
 
 BUILD_FINGERPRINT := google/bluejay/bluejay:14/UP1A.231005.007/10754064:user/release-keys
+
+$(call inherit-product, vendor/google/bluejay/bluejay-vendor.mk)
