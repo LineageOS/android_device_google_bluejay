dad5cfc1d5535380b6ba975871a0f6719aba5b7f
Michael Bestas <mkbestas@gmail.com>
2022-05-18 00:03:07 +0530

    bluejay: Add HbmSVManager RRO overlay
    * Based on redfin
    
    Change-Id: I54d885c233151498372e1b510284d6a297ac3e94

--- device-lineage.mk
+++ device-lineage.mk
@@ -8,6 +8,10 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# HBM
+PRODUCT_PACKAGES := \
+    HbmSVManagerOverlay
+
 # Build necessary packages for vendor
 
 # Audio
