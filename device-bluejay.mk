45f77aba6739edc29d3d6cae406cfff40e5d5036
Alex Hong <rurumihong@google.com>
2022-06-08 21:50:30 +0800

    Jump SVN to 6
    Test: adb shell getprop ro.vendor.build.svn
    Bug: 235336903
    Change-Id: I1616ea9f37fd8903d13bacd8859195bdd857ffc7
    (cherry picked from commit 94268f916367dd859f5e403bde33e9aa7c4e5ef7)
    Merged-In: I1616ea9f37fd8903d13bacd8859195bdd857ffc7

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -117,7 +117,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=5
+    ro.vendor.build.svn=6
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
