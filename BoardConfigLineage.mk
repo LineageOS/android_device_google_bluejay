a1df94e3df4f07c0bd64d76529a1ce1ace71d575
Michael Bestas <mkbestas@lineageos.org>
2022-06-10 05:14:06 +0300

    bluejay: Inline kernel building
    Change-Id: I629c70b74db44d02847212b774982060ab84f1d2

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,4 +6,20 @@
 
 BUILD_BROKEN_DUP_RULES := true
 
+# Kernel
+TARGET_KERNEL_DTBO_PREFIX := dts/
+TARGET_KERNEL_DTBO := google/devices/bluejay/dtbo.img
+TARGET_KERNEL_DTB := \
+    google/devices/bluejay/google-base/gs101-a0.dtb \
+    google/devices/bluejay/google-base/gs101-b0.dtb
+TARGET_KERNEL_SOURCE := kernel/google/bluejay/private/gs-google
+
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/bluejay/vendor_dlkm.modules.load))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(foreach m,$(BOARD_VENDOR_KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/bluejay/vendor_boot.modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(foreach m,$(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
+
+# Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
