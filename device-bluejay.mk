d79a90b48fa01c775c9f5527c03e7bff3925aa67
Steve Berbary <sberbary@google.com>
2022-07-28 18:19:32 +0000

    Update bluejay SVN to 14
    Bug: 238435977
    Change-Id: I92f1e62b7ab689144f3a619343ae1b69cb947b00
    (cherry picked from commit d2f2155b629b611c17bbdbcb3ec3d30516d212ef)
    Merged-In: I92f1e62b7ab689144f3a619343ae1b69cb947b00

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -120,7 +120,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=13
+    ro.vendor.build.svn=14
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
