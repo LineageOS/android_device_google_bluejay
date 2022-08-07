LineageOS/android_device_google_raviole

Set TARGET_KERNEL_SOURCE
Change-Id: I629c70b74db44d02847212b774982060ab84f1d2

BoardConfigLineage.mk
@@ -6,4 +6,8 @@
 
 BUILD_BROKEN_DUP_RULES := true
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/google/raviole
+
+# Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml

LineageOS/android_device_google_bluejay

BoardConfigLineage.mk
@@ -6,4 +6,8 @@
 
 BUILD_BROKEN_DUP_RULES := true
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/google/bluejay
+
+# Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml