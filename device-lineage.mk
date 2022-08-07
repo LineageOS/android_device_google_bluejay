LineageOS/android_device_google_raviole

Set TARGET_PREBUILT_KERNEL
Change-Id: I156a4af77bd619f855f734983b24677531156c4d

device-lineage.mk
@@ -19,6 +19,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
+
 # Parts
 PRODUCT_PACKAGES += \
     GoogleParts

LineageOS/android_device_google_bluejay

device-lineage.mk
@@ -23,6 +23,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES := \
     HbmSVManagerOverlay
 
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/bluejay-kernel/Image.lz4
+
 # Parts
 PRODUCT_PACKAGES += \
     GoogleParts