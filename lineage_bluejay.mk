c54dd54d3ee30639bdc86e38f5e11fb34d612db6
Michael Bestas <mkbestas@lineageos.org>
2022-08-09 22:40:52 +0300

    Update fingerprint/build description from SD2A.220601.003.B1
    Change-Id: I1cd9d0fbd42ce485eb8a5cc79e730bcba560c00a

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
-    PRIVATE_BUILD_DESC="bluejay-user 12 SD2A.220601.003 8804744 release-keys"
+    PRIVATE_BUILD_DESC="bluejay-user 12 SD2A.220601.003.B1 8836721 release-keys"
 
-BUILD_FINGERPRINT := google/bluejay/bluejay:12/SD2A.220601.003/8804744:user/release-keys
+BUILD_FINGERPRINT := google/bluejay/bluejay:12/SD2A.220601.003.B1/8836721:user/release-keys
 
 $(call inherit-product, vendor/google/bluejay/bluejay-vendor.mk)
