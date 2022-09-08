LineageOS/android_device_google_raviole

Add initial Lineage product makefiles
Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

AndroidProducts.mk
@@ -31,7 +31,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_whitefin_pkvm.mk \
     $(LOCAL_DIR)/aosp_whitefin_hwasan.mk \
     $(LOCAL_DIR)/aosp_whitefin_64.mk \
-    $(LOCAL_DIR)/factory_whitefin.mk
+    $(LOCAL_DIR)/factory_whitefin.mk \
+    $(LOCAL_DIR)/lineage_oriole.mk \
+    $(LOCAL_DIR)/lineage_raven.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_slider-userdebug \

LineageOS/android_device_google_bluejay

AndroidProducts.mk
@@ -16,7 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_bluejay.mk \
-    $(LOCAL_DIR)/factory_bluejay.mk
+    $(LOCAL_DIR)/factory_bluejay.mk \
+    $(LOCAL_DIR)/lineage_bluejay.mk
 
 
 COMMON_LUNCH_CHOICES := \