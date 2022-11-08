7724d014aaa53eb0d02020cd7d042e3cc3f6cc9f
Michael Bestas <mkbestas@lineageos.org>
2022-11-08 17:53:59 +0200

    Update fingerprint/build description from TP1A.221105.002
    Change-Id: If03f094ed643cfbb2ac8afac7ac12ff2d9ba62af

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
-    PRIVATE_BUILD_DESC="bluejay-user 13 TP1A.221005.003 9018821 release-keys"
+    PRIVATE_BUILD_DESC="bluejay-user 13 TP1A.221105.002 9080065 release-keys"
 
-BUILD_FINGERPRINT := google/bluejay/bluejay:13/TP1A.221005.003/9018821:user/release-keys
+BUILD_FINGERPRINT := google/bluejay/bluejay:13/TP1A.221105.002/9080065:user/release-keys
 
 $(call inherit-product, vendor/google/bluejay/bluejay-vendor.mk)
