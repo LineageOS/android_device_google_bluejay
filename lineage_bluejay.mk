cf8d3d9e0cc59fcddd582a42068bb5a79dfb6baa
Michael Bestas <mkbestas@lineageos.org>
2023-06-14 23:14:34 +0300

    Update fingerprint/build description from TQ3A.230605.010
    Change-Id: Ifa2d9e4aeb0911b8667536efcfbf8632fea95d69

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
-    PRIVATE_BUILD_DESC="bluejay-user 13 TQ2A.230505.002 9891397 release-keys"
+    PRIVATE_BUILD_DESC="bluejay-user 13 TQ3A.230605.010 10121037 release-keys"
 
-BUILD_FINGERPRINT := google/bluejay/bluejay:13/TQ2A.230505.002/9891397:user/release-keys
+BUILD_FINGERPRINT := google/bluejay/bluejay:13/TQ3A.230605.010/10121037:user/release-keys
 
 $(call inherit-product, vendor/google/bluejay/bluejay-vendor.mk)
