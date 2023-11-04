75bff8a95f3e1fbeeaffce4d2fe720152761edbd
Roshan Pius <rpius@google.com>
2023-11-04 14:58:26 +0000

    Revert "bluejay: Replace NFC app with NFC apex"
    Revert submission 24993894-cherrypicker-L80100000963393177:N49200001411943056
    
    Reason for revert: Causing bootloop with signed builds
    
    Reverted changes: /q/submissionid:24993894-cherrypicker-L80100000963393177:N49200001411943056
    
    Bug: 309117462
    Change-Id: Ie6d5643f77a828f9a4254edd0ecf4fe28460a2f6
    Test: Compiles, device boots up

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -104,7 +104,7 @@ PRODUCT_COPY_FILES += \
 	device/google/bluejay/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	com.android.nfcservices \
+	NfcNci \
 	Tag \
 	android.hardware.nfc-service.st
 
