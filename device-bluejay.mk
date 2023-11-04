041ffd8ea61c819aa02f73440b890101038ab694
Roshan Pius <rpius@google.com>
2023-11-04 19:04:14 +0000

    Revert "bluejay(nfc): Modify NFC overlays for NFC apex"
    Revert submission 2802548-nfc_apex
    
    Reason for revert: Causing bootloop with signed builds
    
    Reverted changes: /q/submissionid:2802548-nfc_apex
    
    Bug: 309117462
    Test: Compiles, device boots up
    (cherry picked from https://android-review.googlesource.com/q/commit:40ba66aca3ed1c1e5d3a7dbbaffa0b73ec4d396c)
    Merged-In: Ia4d272bb55ed7fd9fc12090a6afde96fff84a9da
    Change-Id: Ia4d272bb55ed7fd9fc12090a6afde96fff84a9da

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -111,8 +111,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	com.android.nfcservices \
 	Tag \
-	android.hardware.nfc-service.st \
-	NfcOverlayBluejay
+	android.hardware.nfc-service.st
 
 # SecureElement
 PRODUCT_PACKAGES += \
