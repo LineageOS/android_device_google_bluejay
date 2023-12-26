b7fdfed8930dd136c41921cf2f1b77bedf740709
Aaron Ding <aaronding@google.com>
2023-12-27 00:49:18 +0800

    bluejay: control the kernel by release configuration
    Bug: 317784824
    Change-Id: Id556d4851f03a755f8c062b8be2d023de67125d3
    Signed-off-by: Aaron Ding <aaronding@google.com>

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -20,6 +20,15 @@ $(call inherit-product-if-exists, vendor/google/products/sources_pixel.mk)
 TARGET_KERNEL_DIR ?= device/google/bluejay-kernel
 TARGET_BOARD_KERNEL_HEADERS := device/google/bluejay-kernel/kernel-headers
 
+ifdef RELEASE_GOOGLE_BLUEJAY_KERNEL_VERSION
+TARGET_LINUX_KERNEL_VERSION := $(RELEASE_GOOGLE_BLUEJAY_KERNEL_VERSION)
+endif
+
+ifdef RELEASE_GOOGLE_BLUEJAY_KERNEL_DIR
+TARGET_KERNEL_DIR := $(RELEASE_GOOGLE_BLUEJAY_KERNEL_DIR)
+TARGET_BOARD_KERNEL_HEADERS := $(RELEASE_GOOGLE_BLUEJAY_KERNEL_DIR)/kernel-headers
+endif
+
 $(call inherit-product-if-exists, vendor/google_devices/bluejay/prebuilts/device-vendor-bluejay.mk)
 $(call inherit-product-if-exists, vendor/google_devices/gs101/prebuilts/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/gs101/proprietary/device-vendor.mk)
