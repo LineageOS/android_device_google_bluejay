d6e970d77faf976148c0819e3758c4fd80f79340
Michael Bestas <mkbestas@lineageos.org>
2024-09-04 06:03:07 +0300

    Update fingerprint/build description from AP2A.240905.003.F1
    Change-Id: Id21f023d3f9f98a814b68fa540d984714cb3606c

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
-    PRIVATE_BUILD_DESC="bluejay-user 14 AP2A.240805.005.F1 12043167 release-keys"
+    PRIVATE_BUILD_DESC="bluejay-user 14 AP2A.240905.003.F1 12235381 release-keys"
 
-BUILD_FINGERPRINT := google/bluejay/bluejay:14/AP2A.240805.005.F1/12043167:user/release-keys
+BUILD_FINGERPRINT := google/bluejay/bluejay:14/AP2A.240905.003.F1/12235381:user/release-keys
 
 $(call inherit-product, vendor/google/bluejay/bluejay-vendor.mk)
