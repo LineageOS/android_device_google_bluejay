f5904c9459fa206e0e353eacfa0fbcb62b19f6ea
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- sepolicy/bluejay-sepolicy.mk
+++ sepolicy/bluejay-sepolicy.mk
@@ -1,2 +1,5 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/bluejay/sepolicy/bluejay
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/bluejay/sepolicy/tracking_denials
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
