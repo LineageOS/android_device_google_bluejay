2c8f150001860c9a58a1538ea168275649c8e141
Michael Bestas <mkbestas@lineageos.org>
2025-11-22 13:47:41 +0200

    Update fingerprint/build description from BP3A.250905.014
    Change-Id: I650552939e6806678b6e84102c29b85a0a87ddd2

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -26,8 +26,8 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="bluejay-user 16 BP2A.250705.008.A1 13673213 release-keys" \
-    BuildFingerprint=google/bluejay/bluejay:16/BP2A.250705.008.A1/13673213:user/release-keys \
+    BuildDesc="bluejay-user 16 BP3A.250905.014 13873947 release-keys" \
+    BuildFingerprint=google/bluejay/bluejay:16/BP3A.250905.014/13873947:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
