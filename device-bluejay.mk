9132349c8cbb1616249a7a12dad0715f624f8597
Michael Bestas <mkbestas@lineageos.org>
2026-02-14 21:22:13 +0200

    bluejay: Remove unused soong namespace imports
    Change-Id: I4b0bf91b1b2d1bddd22c888ba2e6c274d0183842

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -45,9 +45,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-PRODUCT_SOONG_NAMESPACES += \
-    device/google/bluejay
-
 # Hide cutout overlays
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
@@ -101,6 +98,10 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/vendor.prop
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.dynamic_sensor_hal
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
 
 # VINTF
 DEVICE_MANIFEST_FILE += \
