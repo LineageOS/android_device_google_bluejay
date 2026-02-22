2547c021f515d4379aff421c267a271970903b43
Michael Bestas <mkbestas@lineageos.org>
2026-02-23 00:17:21 +0200

    bluejay: Convert Aperture overlay to RRO
    Change-Id: I5833e0db244baba0e883027f68bdc8b618a9e026

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -15,9 +15,6 @@ TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 include device/google/gs101/device-shipping-common.mk
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    device/google/bluejay/overlay-lineage
-
 PRODUCT_PACKAGES += \
     DMServiceOverlayVendorBluejay \
     FrameworkResOverlayProductBluejay \
@@ -32,6 +29,9 @@ PRODUCT_PACKAGES += \
     SettingsOverlayGB62Z \
     SettingsOverlayGX7AS \
     SystemUIGoogleOverlayVendorBluejay
+
+PRODUCT_PACKAGES += \
+    ApertureOverlayBluejay
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/product.prop
